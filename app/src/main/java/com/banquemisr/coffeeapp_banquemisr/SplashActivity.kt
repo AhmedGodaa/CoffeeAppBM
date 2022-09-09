905876585d5b1d0dc171110b4ac77024b2d37903
Ahmed Godaa <ana.a7med1140@gmail.com>
2022-09-09 02:37:44 +0200

    Add SplashActivity.kt and drawable ic_logo.png

---
+++ app/src/main/java/com/banquemisr/coffeeapp_banquemisr/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.banquemisr.coffeeapp_banquemisr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
