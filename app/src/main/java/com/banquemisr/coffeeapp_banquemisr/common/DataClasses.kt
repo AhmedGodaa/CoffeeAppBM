7914b212a717f680c4e42b4ca618c1475e604676
shazlee00 <72980547+shazlee00@users.noreply.github.com>
2022-09-12 11:10:11 +0200

    Create DataClasses.kt

---
+++ app/src/main/java/com/banquemisr/coffeeapp_banquemisr/common/DataClasses.kt
@@ -0,0 +1,46 @@
+package com.banquemisr.coffeeapp_banquemisr.common
+
+//which will appear in the menu and preferences screen
+data class Coffee(val name: String, val unitPrice: Double, val imageUrl: String)
+
+//each coffee item after checkout
+data class CoffeeOrder(val name: String, val count: Int, val size: Int=-1, val sugar: Int=-1, val totalPrice: Double)
+
+//holds the user's info
+data class User(val name: String, val address: String)
+
+//the json body
+data class UserOrder(val user: User, val totalPrice: Int, val coffeesList: List<CoffeeOrder>)
+
+
+/*
+JSON example
+{
+    user:{
+        name:""
+        address:""
+    }
+
+    totalPrice:
+
+    coffeesList:[
+        {
+            name:"bla"
+            count:
+            size:
+            sugar:
+            totalPrice: count * unitPrice
+        },
+        {
+            name:"blabla"
+            count:
+            size:
+            sugar:
+            totalPrice: count * unitPrice
+        }
+    ]
+
+
+}
+
+ */
