4cbe1bb78aeef1af6f9d0b9e10b9a2d52c15ceb8
Ahmed Godaa <ana.a7med1140@gmail.com>
2022-09-14 01:48:01 +0200

    Implement OrderViewModel

--- app/src/main/java/com/banquemisr/coffeeapp_banquemisr/presentation/main/MainActivity.kt
+++ app/src/main/java/com/banquemisr/coffeeapp_banquemisr/presentation/main/MainActivity.kt
@@ -2,8 +2,9 @@ package com.banquemisr.coffeeapp_banquemisr.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.view.GravityCompat
 import com.banquemisr.coffeeapp_banquemisr.R
 import com.banquemisr.coffeeapp_banquemisr.common.Constants
 import com.banquemisr.coffeeapp_banquemisr.databinding.ActivityMainBinding
@@ -11,15 +12,27 @@ import com.banquemisr.coffeeapp_banquemisr.domain.model.Coffee
 import com.banquemisr.coffeeapp_banquemisr.presentation.menu.MenuAdapter
 import com.banquemisr.coffeeapp_banquemisr.presentation.menu.MenuListener
 import com.banquemisr.coffeeapp_banquemisr.presentation.order.OrderActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity(), MenuListener {
+class MainActivity : AppCompatActivity(), MenuListener,
+    NavigationView.OnNavigationItemSelectedListener {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 //        getAhmedTheme()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(3).isEnabled = false
+        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem? ->
+            onNavigationItemSelected(
+                item!!
+            )
+        }
 
         val coffees: ArrayList<Coffee> = ArrayList()
         coffees.add(Coffee(name = "Espresso", icon = R.drawable.ic_espresso, unitPrice = 10.0f))
@@ -28,9 +41,9 @@ class MainActivity : AppCompatActivity(), MenuListener {
         coffees.add(Coffee(name = "Mocha", icon = R.drawable.ic_mocha, unitPrice = 35.0f))
         coffees.add(Coffee(name = "Latte", icon = R.drawable.ic_latte, unitPrice = 40.0f))
         val adapter = MenuAdapter(coffees, this)
-        binding.recyclerView.setHasFixedSize(true)
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+//        binding.recyclerView.setHasFixedSize(true)
+//        binding.recyclerView.adapter = adapter
+//        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
 
@@ -40,5 +53,45 @@ class MainActivity : AppCompatActivity(), MenuListener {
         intent.putExtra(Constants.KEY_MENU_NAME, model.name)
         intent.putExtra(Constants.KEY_MENU_PRICE, model.unitPrice)
         startActivity(intent)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_home) {
+            val homeFragment = HomeFragment()
+            val fm = supportFragmentManager
+            val ft = fm.beginTransaction()
+            ft.replace(R.id.fragmentContainer, mainFragment)
+            ft.commit()
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        if (item.itemId == R.id.menu_cart) {
+            val usersFragment = UsersFragment()
+            val fm = supportFragmentManager
+            val ft = fm.beginTransaction()
+            ft.replace(R.id.fragmentContainer, usersFragment)
+            ft.commit()
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        if (item.itemId == R.id.menu_profile) {
+            val groupsFragment = GroupsFragment()
+            val fm = supportFragmentManager
+            val ft = fm.beginTransaction()
+            ft.replace(R.id.fragmentContainer, groupsFragment)
+            ft.commit()
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+
+        if (item.itemId == R.id.menu_home) {
+            val intent = Intent(applicationContext, TimelineActivity::class.java)
+            startActivity(intent)
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        if (item.itemId == R.id.menu_logout) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            signOut()
+        }
+
+        return true
     }
 }
